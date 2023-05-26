327f7a76e82ff8c2e1d59b81a3c25a50c12e8e24
johndward01 <johndward01@gmail.com>
2023-05-26 14:10:56 -0500

    update

--- Method_Overloading_Exercise_02/Methods.cs
+++ Method_Overloading_Exercise_02/Methods.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Method_Overloading_Exercise_02;
 
-namespace Method_Overloading_Exercise_02;
 internal class Methods
 {
+
+    // TODO: Create 2 overloads of the Area method
+    // 1 for a rectangle Area(double length, double width)
+    // 1 for a circle Area(double radius)
+
     /// <summary>
     /// Calculates the Area of the square
     /// </summary>
     /// <param name="side">The length of the side of the square</param>
     /// <returns>The Area</returns>
-    public static double Area(double side)
+    public static double Area(int side)
     {
         return Math.Pow(side, 2);
     }
 
-    // TODO: Create 2 overloads of the Area method above
-    // 1 for a rectangle Area(double length, double width)
-    // 1 for a circle Area(double radius)
+
+
+    // TODO: Create 2 overloads of the Concatenate method 
+    // 1 for taking in 3 strings Concatenate(string str1, string str2, string str3)
+    // 1 for taking an Array of strings Concatenate(string[] strs)
 
     /// <summary>
     /// Combines the strings into 1 string
@@ -32,8 +34,31 @@ internal class Methods
         return str1 + str2;
     }
 
-    // TODO: Create 2 overloads of the Concatenate method above
-    // 1 for taking in 3 strings Concatenate(string str1, string str2, string str3)
-    // 1 for taking an Array of strings Concatenate(string[] strs)
 
+    // TODO: Create an overload of the SetCoordinate Method 
+    // for taking 3 parameters SetCoordinate(int x, int y, int z)
+    // hint: you will need to use the Coordinate class below
+
+    /// <summary>
+    /// Sets a Coordinate
+    /// </summary>
+    /// <param name="x">Represents the x-axis</param>
+    /// <param name="y">Represents the y-axis</param>
+    /// <returns>An instance of a Coordinate class with the X and Y fields set</returns>
+    public static Coordinate SetCoordinate(int x, int y)
+    {
+        return new Coordinate(x, y);
+    }
+}
+
+
+public class Coordinate
+{
+    public Coordinate(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    double X;
+    double Y;
 }
