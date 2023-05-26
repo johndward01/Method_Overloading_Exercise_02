84a86680bc542c58efa12d12bc487d2381a0f8ec
johndward01 <johndward01@gmail.com>
2023-05-26 13:58:12 -0500

    update

---
+++ Method_Overloading_Exercise_02/Methods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_Exercise_02;
+internal class Methods
+{
+    /// <summary>
+    /// Calculates the Area of the square
+    /// </summary>
+    /// <param name="side">The length of the side of the square</param>
+    /// <returns>The Area</returns>
+    public static double Area(double side)
+    {
+        return Math.Pow(side, 2);
+    }
+
+    // TODO: Create 2 overloads of the Area method above
+    // 1 for a rectangle Area(double length, double width)
+    // 1 for a circle Area(double radius)
+
+    /// <summary>
+    /// Combines the strings into 1 string
+    /// </summary>
+    /// <param name="str1">The first string</param>
+    /// <param name="str2">The second string</param>
+    /// <returns>The combined string</returns>
+    public static string Concatenate(string str1, string str2)
+    {
+        return str1 + str2;
+    }
+
+    // TODO: Create 2 overloads of the Concatenate method above
+    // 1 for taking in 3 strings Concatenate(string str1, string str2, string str3)
+    // 1 for taking an Array of strings Concatenate(string[] strs)
+
+}
--- Method_Overloading_Exercise_02/Program.cs
+++ Method_Overloading_Exercise_02/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Complete all of the TODO's here AND in the Methods class
+
+
+// TODO: Call the rectangle Area method overload with 2 arguments 
+
+
+// TODO: Call the circle Area method overload with 1 argument
+
+
+// TODO: Call the Concatenate method overload with 3 string arguments 
+
+
+// TODO: Call the Concatenate method overload with a string[] of arguments 
+
+
+// TODO: Call the SetCoordinate method overload with 3 arguments 
+
+
